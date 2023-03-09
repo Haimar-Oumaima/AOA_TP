2813c367547f93073d2d4a6b519fe3df56296dd1
Oumaima HAIMAR <oumaima.haimar@ens.uvsq.fr>
2023-03-09 11:48:49 +0100

    modification driver avec prof

--- driver.c
+++ driver.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void baseline (unsigned n , float x [ n ] ,
+#define NB_METAS 31
+
+extern uint64_t rdtsc();
+
+
+void baselineKernel (unsigned n , float x [ n ] ,
 const float y [ n ] ,
 const float z [ n ][ n ]) {
 
@@ -11,16 +16,34 @@ for ( i =0; i < n ; i ++)
 x [ i ] += y [ j ] * z [ i ][ j ];
 }
 
+static int cmp_uint64 (const void *y, const void *z) {
+  const uint64_t vy = *((uint64_t *) y);
+  const uint64_t vz = *((uint64_t *) z);
+    if (vy < vz) return -1; 
+    if (vy > vz) return 1;
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   // Vérifier si les arguments sont corrects
-  if (argc != 3) {
-    printf("Usage: %s [taille_problème] [nom_fichier_sortie]\n", argv[0]);
+  if (argc != 4) {
+   // printf("Usage: %s [taille_problème] [nom_fichier_sortie]\n", argv[0]);
+   printf("Usage: %s [taille_problème] [nb_warmup] [nb_repetitions] \n", argv[0]);
     exit(1);
   }
 
   //1- Récupérer les paramètres de la ligne de commande
   int taille = atoi(argv[1]);
-  char *nom_fichier_sortie = argv[2];
+  //char *nom_fichier_sortie = argv[2];
+  const unsigned repw = atoi (argv[2]); /* nb de warmup repetitions */
+  const unsigned repm = atoi (argv[3]); /* nb de repetitions during measurement */
+
+  uint64_t tdiff [NB_METAS];
+  unsigned m;
+  for (m=0; m<NB_METAS; m++) {
+    printf ("Metarepetition %u/%d: running %u warmup instances and %u measure instances\n", m+1, NB_METAS, m == 0 ? repw : 1, repm);
+
+  unsigned i;
 
   // 2- Allouer les tableaux nécessaires
   float *x = (float *)calloc(taille, sizeof(float));
@@ -30,7 +53,7 @@ int main(int argc, char *argv[]) {
     z[i] = (float *)malloc(taille * sizeof(float));
   }
 
-  // 3- Initialiser les tableaux
+  // 3- Initialiser les tableaux (pas de fct init encr)
   for (int i = 0; i < taille; i++) {
     x[i] = 0.0;
     y[i] = i;
@@ -41,23 +64,40 @@ int main(int argc, char *argv[]) {
   }
 
   //4- Appeler la fonction kernel
-  baseline(taille, x, y, (const float (*)[])z);
+  //baselineKernel(taille, x, y, (const float (*)[])z);
 
-  // Ouvrir le fichier de sortie
-  FILE *fichier_sortie;
-  fichier_sortie = fopen(nom_fichier_sortie, "w");
-  if (fichier_sortie == NULL) {
-    printf("Erreur: impossible d'ouvrir le fichier %s\n", nom_fichier_sortie);
-    exit(1);
+  /* warmup (repw repetitions in first meta, 1 repet in next metas) */
+  if (m == 0) {
+    for (i=0; i<repw; i++)
+      baselineKernel (taille, x, y, (const float (*)[])z);
+  } else {
+      baselineKernel (taille, x, y, (const float (*)[])z);
   }
 
-  // 5- Ecrire les valeurs écrites par la fonction kernel dans le fichier
-  for (int i = 0; i < taille; i++) {
-    fprintf(fichier_sortie, "%f\n", x[i]);
+  /* measure repm repetitions */
+  const uint64_t t1 = rdtsc ();
+  for (i=0; i<repm; i++) {
+    baselineKernel (taille, x, y, (const float (*)[])z);
   }
 
-  // Fermer le fichier de sortie
-  fclose(fichier_sortie);
+  const uint64_t t2 = rdtsc ();
+  tdiff[m] = t2 - t1;
+
+  // // Ouvrir le fichier de sortie
+  // FILE *fichier_sortie;
+  // fichier_sortie = fopen(nom_fichier_sortie, "w");
+  // if (fichier_sortie == NULL) {
+  //   printf("Erreur: impossible d'ouvrir le fichier %s\n", nom_fichier_sortie);
+  //   exit(1);
+  // }
+
+  // // 5- Ecrire les valeurs écrites par la fonction kernel dans le fichier
+  // for (int i = 0; i < taille; i++) {
+  //   fprintf(fichier_sortie, "%f\n", x[i]);
+  // }
+
+  // // Fermer le fichier de sortie
+  // fclose(fichier_sortie);
 
   //6 Désallouer les tableaux
   free(x);
@@ -66,6 +106,20 @@ int main(int argc, char *argv[]) {
     free(z[i]);
   }
   free(z);
+}
+  const unsigned nb_itr = taille * taille * repm;
+  qsort (tdiff, NB_METAS, sizeof tdiff[0], cmp_uint64);
+  printf("Minimum : %llu RDTSC-cycles (%.2f par itération) \n", tdiff[0], (float) tdiff[0]/nb_itr);
+  printf("Median : %llu RDTSC-cycles (%.2f par itération) In", tdiff[NB_METAS/2], (float) tdiff[NB_METAS/2]/nb_itr);
+  const float stabilite = (tdiff[NB_METAS/2] - tdiff[0]) * 100.0f / tdiff[0];
 
-  return 0;
+  if (stabilite >= 10)
+    printf("Bad Stability : %.2f %%\n", stabilite);
+  else if ( stabilite >= 5 )
+    printf("Average Stability : %.2f %%\n", stabilite);
+  else
+    printf ("Good Stability : %.2f %%\n", stabilite);
+
+    return 0;
+  
 }
